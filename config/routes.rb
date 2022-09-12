ca33d9fb2213131fea410a2253681d2135b08072
Kyle <khartson2017@gmail.com>
2022-09-12 10:49:25 -0400

    creates sessions controller for sign-in flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  
+ 
+  # model routes
   resources :users
+
+  # sign in flow routes
+  post '/signup', to: 'users#create'
+
+
+  # post '/login', to: 'sessions#create'
+  # delete '/logout', to: 'sessions#destroy'
+  # get '/me', to: 'users#show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
