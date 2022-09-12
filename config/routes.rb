ff448ee0b71ca1094a1c05a492d811391cf61e9f
Kyle <khartson2017@gmail.com>
2022-09-12 11:19:25 -0400

    added get /me route, todo: add user controller logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
 
   # sign in flow routes
   post '/signup', to: 'users#create'
-
+  get '/me', to: 'users#show'
 
   # post '/login', to: 'sessions#create'
   # delete '/logout', to: 'sessions#destroy'
-  # get '/me', to: 'users#show'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
