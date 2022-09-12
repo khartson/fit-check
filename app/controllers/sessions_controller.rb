c560cb6497b2c2d854a8f32fa2935f1ccf8b0f12
Kyle <khartson2017@gmail.com>
2022-09-12 14:54:22 -0400

    implemented logout sessions#destroy logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
 
+  # session create
+  # handles user login + authentication
+  # if auth, updates session hash and renders user profile
+  # if not auth, returns json error
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
@@ -7,6 +11,20 @@ class SessionsController < ApplicationController
       return render json: user, status: :created
     else
       render json: { errors: ['Invalid username or password'] }, status: :unauthorized
+    end 
+  end 
+
+  # sessions destroy
+  # handles user logout
+  # deletes user_id from session hash
+  # returns no_content or json error if not
+  #  logged in 
+  def destroy
+    if session[:user_id]
+      session.delete :user_id
+      head :no_content
+    else
+      render json: { errors: ['You are not logged in'] }, status: :unauthorized
     end 
   end 
 
